66dd9b3b874afdd56ac447d3856eafb9ede4d48c
Udam Liyanage <udamsliyanage@gmail.com>
2021-11-28 18:07:09 +0530

    Service provider cleaned up

--- src/LaravelSendlkServiceProvider.php
+++ src/LaravelSendlkServiceProvider.php
@@ -13,32 +13,14 @@ class LaravelSendlkServiceProvider extends ServiceProvider
     public function boot()
     {
         /*
-         * Optional methods to load your package assets
+         * Optional methods to load package assets
          */
-        // $this->loadTranslationsFrom(__DIR__.'/../resources/lang', 'laravel-sendlk');
-        // $this->loadViewsFrom(__DIR__.'/../resources/views', 'laravel-sendlk');
         $this->loadMigrationsFrom(__DIR__.'/../database/migrations');
-        // $this->loadRoutesFrom(__DIR__.'/routes.php');
 
         if ($this->app->runningInConsole()) {
             $this->publishes([
                 __DIR__.'/../config/config.php' => config_path('laravel-sendlk.php'),
             ], 'config');
-
-            // Publishing the views.
-            /*$this->publishes([
-                __DIR__.'/../resources/views' => resource_path('views/vendor/laravel-sendlk'),
-            ], 'views');*/
-
-            // Publishing assets.
-            /*$this->publishes([
-                __DIR__.'/../resources/assets' => public_path('vendor/laravel-sendlk'),
-            ], 'assets');*/
-
-            // Publishing the translation files.
-            /*$this->publishes([
-                __DIR__.'/../resources/lang' => resource_path('lang/vendor/laravel-sendlk'),
-            ], 'lang');*/
 
             // Registering package commands.
             $this->commands([
