019c4733a5d9185c7b2141a642cf47f1c1bae2dd
Udam Liyanage <udamsliyanage@gmail.com>
2021-11-28 14:33:21 +0530

    Message Log base model

---
+++ src/Models/MessageLog.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Althinect\LaravelSendlk\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class MessageLog extends Model
+{
+    use HasFactory;
+
+    protected $guarded = [];
+}
