96e268b1850921f1c3b0a424d25893812e5946df
Udam Liyanage <udamsliyanage@gmail.com>
2021-11-28 12:35:11 +0530

    Method to send a message to single phone number

--- src/LaravelSendlk.php
+++ src/LaravelSendlk.php
@@ -2,7 +2,37 @@
 
 namespace Althinect\LaravelSendlk;
 
+use Illuminate\Support\Facades\Http;
+
 class LaravelSendlk
 {
-    // Build your next great package.
+
+    private $statusCode;
+    
+
+    public function __construct()
+    {
+        $this->statusCode = 0;
+        
+    }
+
+    public function send($phoneNumber, $message)
+    {
+        $response = Http::withToken(config('laravel-sendlk.api_token'))->post('https://sms.send.lk/api/v3/sms/send', [
+            'recipient' => $phoneNumber,
+            'sender_id' => config('laravel-sendlk.sender_id'),
+            'message' => $message,
+        ]);
+
+        $this->statusCode = $response->status();
+    }
+
+    public function messageStatus() 
+    {
+        if ($this->statusCode == 200) {
+            return true;
+        } 
+
+        return false;
+    }
 }
