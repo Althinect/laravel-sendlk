e43bcdc399028900870c2fad09f748a88d0cbe93
Udam Liyanage <udamsliyanage@gmail.com>
2021-11-28 18:06:26 +0530

    Options for prune logs command

--- src/Console/PruneLogs.php
+++ src/Console/PruneLogs.php
@@ -7,14 +7,27 @@ use Illuminate\Console\Command;
 
 class PruneLogs extends Command
 {
-    protected $signature = 'laravel-sendlk:prune';
+    protected $signature = 'laravel-sendlk:prune 
+                            {--only-successful-logs : Delete only successful logs} 
+                            {--only-errors} : Delete only error logs';
 
     protected $description = 'Prune message log of Laravel SendLK package';
 
     public function handle()
     {
-        $this->info('Pruning all message logs!');
-        MessageLog::truncate();
-        $this->info('All message logs pruned!');
+        $this->info('Pruning message logs!');
+        if ($this->option('only-successful-logs') && $this->options('only-errors')) {
+            MessageLog::truncate();
+            $this->info('All message logs pruned!');
+        } else if ($this->option('only-successful-logs')) {
+            MessageLog::where('success', true)->delete();
+            $this->info('Successful message logs pruned!');
+        } else if ($this->option('only-errors')) {
+            MessageLog::where('success', false)->delete();
+            $this->info('Error message logs pruned!');
+        } else {
+            MessageLog::truncate();
+            $this->info('All message logs pruned!');
+        }
     }
 }
