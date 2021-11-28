693ac230aa2b15ab7469928ba55d9aa7e41f52ce
Udam Liyanage <udamsliyanage@gmail.com>
2021-11-28 17:44:19 +0530

    Message Logs migration

---
+++ database/migrations/2021_11_28_115034_create_message_logs_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateMessageLogsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('message_logs', function (Blueprint $table) {
+            $table->id();
+            $table->boolean('success')->default(true);
+            $table->string('phone_number')->nullable();
+            $table->json('api_response')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('message_logs');
+    }
+}
